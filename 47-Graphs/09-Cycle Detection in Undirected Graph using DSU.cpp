62b2f062d193a3cedae5091ed54a9668c205e1b8
codebysumit <indianstudentsumit@gmail.com>
2021-07-26 20:20:06 +0530

    add 09-Cycle Detection in Undirected Graph using DSU and update graph file

---
+++ 47-Graphs/09-Cycle Detection in Undirected Graph using DSU.cpp
@@ -0,0 +1,83 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+const int N=1e5+2;
+std::vector<int> parent(N);
+std::vector<int> sz(N);
+
+void make_set(int v){
+    parent[v]=v;
+    sz[v]=1;
+    
+}
+
+int find_set(int v){
+    if(v == parent[v])
+        return v;
+    return parent[v]=find_set(parent[v]);
+}
+
+void union_sets(int a, int b){
+    a=find_set(a);
+    b=find_set(b);
+    if(a!=b){
+        if(sz[a]<sz[b])
+            swap(a,b);
+        parent[b]=a;
+        sz[a]+=sz[b];
+    }
+}
+
+int main() {
+	for (int i = 0; i < N; i++) {
+	    make_set(i);
+	}
+	
+	int n,m;
+	cin>>n>>m;
+	std::vector<std::vector<int>> edges;
+	for (int i = 0; i < m; i++) {
+	    int u,v;
+	    cin>>u>>v;
+	    edges.push_back({u,v});
+	}
+	bool cycle=false;
+	for (auto i : edges) {
+	    int u=i[0];
+	    int v=i[1];
+	    int x=find_set(u);
+	    int y=find_set(v);
+	    
+	    if(x==y)
+	        cycle=true;
+	    else
+	        union_sets(u,v);
+	}
+	
+	if(cycle){
+	   std::cout << "cycle is parent" << std::endl;
+	}
+	else{
+	   std::cout << "cycle is not parent" << std::endl;
+	}
+	return 0;
+}
+
+// input:
+// 4 4
+// 0 1
+// 1 2
+// 2 3
+// 3 0
+
+// output:
+// cycle is parent
+
+// input:
+// 4 2
+// 0 1
+// 2 3
+
+// output:
+// cycle is not parent
+
